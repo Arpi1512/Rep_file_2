231fc23da35775b32a851c4959411d76c52c2bcb
Ar <arpikh2@gmail.com>
2021-01-05 17:56:10 +0400

    new code

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import Button from "./components/Button";
+import Toolbar from "./components/Toolbar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    buttonValue: "First Button",
+    toolbarTitle: "Title",
+  };
+
+  onButtonClick = () => {
+    this.setState({ buttonValue: "Second Button" }, () => {
+      this.onChangeToolbarTitleClick();
+    });
+  };
+
+  onChangeToolbarTitleClick = () => {
+    this.setState({
+      toolbarTitle: `Button text changed to ${this.state.buttonValue}`,
+    });
+  };
+  render() {
+    return (
+      <div className="App">
+        <Toolbar title={this.state.toolbarTitle} />
+        <button onClick={this.onButtonClick}>Change Values</button>
+        <Button value={this.state.buttonValue} />
+      </div>
+    );
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ my-app/src/components/Toolbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Toolbar extends React.Component {
+  render() {
+    return <div>{this.props.title}</div>;
+  }
+}
+
+export default Toolbar;
